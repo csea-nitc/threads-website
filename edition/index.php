d9de6bd6c3cb3ab76ab430a606d092a04944dc20
123arjunsuresh <123arjunsuresh@gmail.com>
2018-08-06 02:38:15 -0400

    aug 6

--- edition/index.php
+++ edition/index.php
@@ -28,6 +28,7 @@ $cureditrow=$curedit->fetch_assoc();
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 	<link href="/styles/post.css" rel="stylesheet"> 
+ <link href="/styles/v-style.css" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
 
@@ -39,35 +40,40 @@ $cureditrow=$curedit->fetch_assoc();
 	
 		<body>
 		<!-- <div class="header">-->
-
-	<div class="col l12">
+<div class="row threads-ribbon" style="background : url('/styles/topbar.png'); background-size:cover; ">
+    <div class="col l12 m12 s12">
  
-										<img class="l12 threads-ribbon" src="/styles/topbar.png"/> 
-			
-		<img class="responsive-img threads-logo" src="/styles/threads-logo.png">
-		</div>
-			
+                                        <!--<img class="threads-ribbon" style="background-size:cover" src="/styles/topbar.png"/>--> 
+            
+        <img class="responsive-img threads-logo" src="/styles/threads-logo.png">
+        </div>
+    </div>
+            
  
 <div class="row">
-	
-		<div class="col s2 offset-s1">
-									<div class="row">
-									
-										<div id="article-nav">
-															
-																<div class="editiontitle" id="editiontitle">ARTICLES </div>
-                                  
-										</div>
-
-	
-
-											</div>
-									<div class="row">
-
-									<div id="edition-nav">
-								      <!-- Dropdown Trigger -->
-  <a class='dropdown-trigger btn editiontitle grey darken-2' href='#' data-target='dropdown1'>Editions</a>
-                     <!-- Dropdown Structure -->
+ 
+    <div class="container">
+    
+        <div class="col s2 l2 m2">
+                                    <div class="row">
+                                    
+                                        <div id="article-nav">
+                                                            
+                                                                <div class="editiontitle" id="editiontitle">ARTICLES </div>
+ 
+                                        </div>
+ 
+    
+ 
+                                            </div>
+                                    <div class="row">
+ 
+                                    <div id="edition-nav">
+                                      
+ 
+  <a class='dropdown-trigger btn editiontitle' href='#' data-target='dropdown1'>Editions</a>
+ 
+                     
                      
   <ul id="dropdown1" class="dropdown-content">
 																<?php 
@@ -104,89 +110,103 @@ $cureditrow=$curedit->fetch_assoc();
 									</div>
 									</div>
 
-	<div class="col s8">
-				<div class="row">
-					
-					<div class="col offset-s6 s4">
-						<div class="edyear">
-							
-						<span class="top-edition-text" id="top-edition-text">AUG</span>
-										<span class="top-edition-text">.</span>
-						<span class="top-edition-year-text" id="top-edition-year">2018</span>
-				
-						</div>
-						</div>
-		</div>
-		<div class="row">
-			<div class="col">
-									<div class="article-box" id="article-box">
-										<div class="title-box" id="title-box">
-										</div>
-								 <div class="authordetails" id="authordetails">
-													<div class="authorphoto" id="authorphoto">
-													</div>
-													<div class="authorname" id="authorname">Arjun Suresh
-													</div>
-													<div class="authorbio" id="authorbio">Something Something
-													</div>
-										</div>
-											
-										<div class="content-box" id="content-box">
-										</div>
-										
-			
-		
-		</div>
-			</div>
-				
-				
-									</div>
-
-		</div>
-	</div>
-	
-
-<div class="z-depth-2 newfooter" style="background : url('/webimages/bottombar.png'); background-size:cover; ">
-  <div class='container row show-on-large hide-on-med-and-down ' style='padding-top:1%;padding-bottom:1%; ' >
-   
-      <div class="col m6 s6">
-        <img class="responsive-img" src="/webimages/About.png">
-        <img class="responsive-img" src="/webimages/CSEA.png">
-        <img class="responsive-img" src="/webimages/CSED.png">
-      </div>
-          <div class="col m3 s12">
-        <img class="responsive-img" src="/webimages/copyright.png">
+	    <div class="col l10 m10 s10">
+                <div class="row">
+                    
+                    <div class="col offset-s6 s4">
+                        <div class="edyear">
+                            
+                        <span class="top-edition-text">AUG</span>
+                                        <span class="top-edition-text">.</span>
+                        <span class="top-edition-year-text">2018</span>
+                
+                        </div>
+                        </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                                    <div class="article-box" id="article-box">
+                                        <div class="title-box" id="title-box">
+                                        </div>
+                                 <div class="authordetails" id="authordetails">
+                                                    <div class="authorphoto" id="authorphoto">
+                                                    </div>
+                                                    <div class="authorname" id="authorname">Arjun Suresh
+                                                    </div>
+                                                    <div class="authorbio" id="authorbio">Something Something
+                                                    </div>
+                                        </div>
+                                            
+                                        <div class="content-box" id="content-box">
+                                        </div>
+                                        
+            
+        
+        </div>
+            </div>
+                
+                
+                                    </div>
+ 
+        </div>
     </div>
-      <div class="col m1 s6">
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    </div>
+    
+ 
+<div class="row" style="background : url('/webimages/bottombar.png'); background-size:cover; ">
+  <div class="container" style="padding-top:1%;padding-bottom:3%; " >
+ 
+      <div class="col l4 m6 s12 center">
+        <img class="responsive-img col s4" src="/webimages/About.png" style="padding:1px;" >
+        <img class="responsive-img col s4" src="/webimages/CSEA.png" style="padding:1px;">
+        <img class="responsive-img col s4" src="/webimages/CSED.png" style="padding:1px;">
+      </div>
+      <div class="col l4 m6 s12 center" >
+        <h4 style="font-family:'webfontregular'; font-weight:350; color: #FFFFFF; font-size:49.5px;">threads.</h4>
+      </div>
+        <div class="col l4 m6 s12 center">
+        <div class="">
+      <div class="col s6">
         <img class="responsive-img" src="/webimages/csea-logo.png">
-    </div>
-      <div class="col m1 s6">
+      </div>
+      <div class="col s6">
          <img class="responsive-img" src="/webimages/CSED-logo.png">
+        </div>
+            </div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+        </div>
+ 
+ 
+ 
+ 
+    </div>
+      <div class="col s12">
+        <br>
+        <hr>
       </div>
+      <div class="col l12 m12 s12 center">
+                <img class="responsive-img" src="/webimages/copyright.png">
+        </div>
+ 
   </div>
-  
-  
-   <div class='container row show-on-medium-and-down hide-on-large-only' style='padding-top:1%;padding-bottom:1%; margin:0 auto;' >
-   
-      <div class="col m12 s12" >
-        <img class="responsive-img" src="/webimages/About.png" style='padding:2px;'>
-        <img class="responsive-img" src="/webimages/CSEA.png" style='padding:2px;'>
-        <img class="responsive-img" src="/webimages/CSED.png" style='padding:2px;'>
-      </div>
-     
-     
-              <div class="col m1 s3 offset-s2">
-        <img class="responsive-img" src="/webimages/csea-logo.png">
-    </div>
-      <div class="col m1 s3 offset-s2">
-         <img class="responsive-img" src="/webimages/CSED-logo.png">
-      </div>
-       <div class="col m3 s12" style='padding:3px;bottom:10px; margin: 1px auto;'>
-        <img class="responsive-img" src="/webimages/copyright.png">
-    </div>
-
   </div>
-    </div>
+    
 
 		<script>
   document.addEventListener('DOMContentLoaded', function() {
