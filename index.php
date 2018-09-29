4217466512ab2dbf1a7232eaa5e61ee9e453ea21
CSEA <csea@nitc.ac.in>
2018-09-29 20:39:10 +0530

    favicon

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $edit=$conn->query($sql);
 
 	<head profile="http://www.w3.org/2005/10/profile" >
 		<title>Threads</title>
-		<link rel="icon"       type="image/png" href="/webimages/favicon.png"> 
+		<link rel="icon"       type="image/png" href="/webimages/favicon.png" /> 
 		<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 		<meta content="utf-8" http-equiv="encoding">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
