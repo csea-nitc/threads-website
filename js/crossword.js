4b04ef16322258981c32801fc4baaf82bc08d7c0
CSEA <csea@nitc.ac.in>
2018-09-29 21:51:33 +0530

    ts

--- js/crossword.js
+++ js/crossword.js
@@ -237,9 +237,10 @@
 							};
 							
 							
-								if(hasOffset===false)
+								if($(light).empty())
 								{
-								$(light).addClass('entry-' +puzz.data[x-1].position).append('<input maxlength="1" val="" type="text" tabindex="-1" />');};
+		$(light).addClass('entry-' +puzz.data[x-1].position).append('<input maxlength="1" val="" type="text" tabindex="-1" />');
+								};
 
 								$(light).addClass(' position-' + (x-1) );
 							
