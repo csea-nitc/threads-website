340d4ea96648486b777dbbaf7fa14f0cd3df079b
csea-nitc <csea@nitc.ac.in>
2018-10-01 11:05:46 +0530

    changed cr style

--- crossword.php
+++ crossword.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>-->
 	<script src="/js/jquery.js"></script>
 <script src="/js/crossword.js"></script>
 
