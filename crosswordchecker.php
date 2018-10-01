1722e8b712d0df27b0710299a6ec4e57370d1b51
=csea <=csea@nitc.ac.in>
2018-10-01 14:12:52 +0530

    all this launch

--- crosswordchecker.php
+++ crosswordchecker.php
@@ -3,7 +3,7 @@ $ans=$_POST["ans"];
 
 $ans=strtolower($ans);
 //add answer here 
-if($ans=="biinputnnacfhardwareueyeinxcommentdcaenestedwnetworkieotocharnidmlbreakolncoutzadeclarec")
+if($ans=="biinputnnacfhardwareueyeinxcommentdcaenestedwnetworkieotocharnidmlbreakoilncoutzadeclarec")
        
 
 {echo "correct";}
