0f478fefad029ca542497dd2978f4ee3a399888e
Arjun Suresh <123arjunsuresh@gmail.com>
2018-12-14 20:30:16 +0530

    crossword checker modified to check MD5 of answer than plain text .

--- crosswordchecker.php
+++ crosswordchecker.php
@@ -3,8 +3,7 @@ $ans=$_POST["ans"];
 
 $ans=strtolower($ans);
 //add answer here 
-if($ans=="biinputnnacfhardwareueyeinxcommentdcaenestedwnetworkieotocharnidmlbreakoilncoutzadeclarec")
-       
+if(md5($ans)=="85d019e7fcd881ca75d586e23cdd84b5")
 
 {echo "correct";}
 else{
