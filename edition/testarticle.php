fd8059d4dd8a2433f3eaee618222d9aa4a1de3d1
Arjun Suresh <123arjunsuresh@gmail.com>
2018-12-14 20:15:31 +0530

    Added if condition to prev editions and removed many stale files from repo

--- edition/testarticle.php
+++ edition/testarticle.php
@@ -1,37 +1,6 @@
+ <?php
+$va=$_POST;
 
-
-
-<?php
-
-function isMobile() {
-    
-	
-	return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-}
-
-
-if(isMobile()  && false)
-{
-	include 'mobilepage.php';
-	
-}
-
-else{
-
-include '../common/con.php';
-  
-$id=-2;
-$sql="SELECT * FROM edition where visible=1 order by  year desc,month desc";
-$edit=$conn->query($sql);  
-
-
-$sql="SELECT * FROM edition ";
-$curedit=$conn->query($sql);
-$cureditrow=$curedit->fetch_assoc();
-
-
-
-  
 ?>
 
 	<html>
@@ -44,381 +13,202 @@ $cureditrow=$curedit->fetch_assoc();
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
-	<link href="/styles/post.css" rel="stylesheet"> 
- <link href="/styles/v-style.css" rel="stylesheet">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+		<link href="/styles/post.css" rel="stylesheet">
+		<link rel="icon" href="/webimages/favicon.png?v=1.1">
+		<link href="/styles/v-style.css" rel="stylesheet">
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
-	
-		<body>
+
+	<body>
 		<!-- <div class="header">-->
-<div class="row threads-ribbon" style="background : url('/styles/topbar.png'); background-size:cover; ">
-    <div class="col l12 m12 s12">
- 
-                                        <!--<img class="threads-ribbon" style="background-size:cover" src="/styles/topbar.png"/>--> 
-            
-        <img class="responsive-img threads-logo" src="/styles/threads-logo.png">
-        </div>
-    </div>
-            
- 
-<div class="row">
- 
-    <div class="container">
-    
-        <div class="col s2 l2 m2">
-					<div id="floatsidebar">
-                                    <div class="row">
-                                    <div class="col s12 l12 m12">
-                                        <div id="article-nav" class="row">
-                                                               <div class="editiontitle" id="editiontitle">Articles </div>
- 
-                                      <div id="navbarentry-wrap">
-																				
-																					</div>
-																			
-																			</div>
- 
-    
- 
-                                            </div>
+		<div class="row threads-ribbon" style="background : url('/styles/topbar.png'); background-size:cover; ">
+			<div class="col l12 m12 s12">
+
+				<!--<img class="threads-ribbon" style="background-size:cover" src="/styles/topbar.png"/>-->
+
+				<a href="/"><img class="responsive-img threads-logo" src="/styles/threads-logo.png"></a>
+			</div>
+		</div>
+
+
+		<div class="boxcon row">
+
+			<div class="s11 offset-s1 col">
+
+				<div class="col s3">
+
+
+					<div class="edyear">
+
+						<span class="top-edition-text" id="ednametxt">JAN</span>
+
+						<span class="top-edition-year-text" id="edyeartxt">2018</span>
+
 					</div>
-                                    <div class="row">
- <div class="col s12 m12 l12">
-                                    <div id="edition-nav" class="row">
-                                      
- 
-  <div class="editiontitle" id="editiontitle">Editions </div>
-   <div id="navbarentry-wrap">
-																				
-																					
-                     
-                     
 
-																<?php 
-    $i=0;
-																while($w=$edit->fetch_assoc())
-																{
-                                  
-                                  if($i==0)
-                                  {
-                                    $curname=substr($w["name"],0,3);
-                                    $curyear=$w["year"];
-                                    $i=1;
-                                  }
-                                  $curid=$w["id"];
-                                  $cname=$w["name"];
-                                  $cyear=$w["year"];
-																			?>
-                                  
+					<div id="floatsidebar">
+						<div class="row">
+							<div class="col s12 l12 m12">
+								<div id="article-nav" class="row">
+									<div class="editiontitle" id="editiontitle">Articles </div>
 
-		
-    <div class="navbaritem" onclick="initpage(<?php echo $curid; ?>,-1,true,this.getAttribute('edname'),this.getAttribute('eyear'));" edname="<?php echo  substr($cname,0,3);; ?>" eyear="<?php echo  $cyear; ?>"><?php  echo $w["name"];	?></div>
-    
-  
-        
-												
+									<div id="navbarentry-wrap">
+										<div id="navbarwrapwrap">
 
-																<?php
-
-
-
-																}
-	$va=$_POST;
-																?>
- 
+										</div>
 									</div>
 
-					</div>
-																			</div>
-																			
+								</div>
+
+
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="col s12 m12 l12">
+
+
+								<div id="edition-nav" class="row">
+
+
+									<div class="editiontitle" id="editiontitle">Editions </div>
+									<div id="navbarentry-wrap">
+
+
+
+
+
+									</div>
+
+								</div>
+							</div>
+
 						</div>
 					</div>
-									</div>
-					
-
-	    <div class="col l10 m10 s10">
-                <div class="row">
-                    
-                    <div class="col offset-s9 s3">
-                        <div class="edyear">
-                            
-                        <span class="top-edition-text">JAN</span>
-                                        <span class="top-edition-text">.</span>
-                        <span class="top-edition-year-text">20XX</span>
-                
-                        </div>
-                        </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                                    <div class="article-box" id="article-box">
-                                        <div class="title-box" id="title-box">
-																					
-																				<?php echo $va["title"]; ?>	
-                                        </div>
-                                 <div class="authordetails" id="authordetails">
-                                                   <div class="authornameandphoto">
-                                                    <span class="authorname" id="authorname"><?php echo $va["authorname"]; ?>
-                                                    </span>
-																	 											
-																											<img height="50" width="50" class="circle authorphoto responsive-img" id="authorphoto" src="<?php echo $va["authorphoto"]; ?>"/>
-																	
-																										 
-																										 <div class="authorbio" id="authorbio"><?php echo $va["authorbio"]; ?>
-                                                    </div> </div>
-                                        </div>
-                                            
-                                        <div class="content-box" id="content-box">
-																					
-																					<?php echo $_POST["content"]; ?>
-                                        </div>
-                                        
-            
-        
-        </div>
-            </div>
-                
-                
-                                    </div>
- 
-        </div>
-    </div>
- 
- 
- 
- 
- 
- 
- 
- 
- 
-    </div>
-    
- 
-<div class="row" style="background : url('/webimages/bottombar.png');margin-bottom:0px;padding-top:29px;background-size:cover; ">
-  <div class="container" style="padding-top:1%;padding-bottom:3%;z-index:8;" >
- 
-      <div class="col l4 m6 s12 center">
-        <img class="responsive-img col s4" src="/webimages/About.png" style="padding:1px;" >
-		<a href="http://assoc.cse.nitc.ac.in/">		<img class="responsive-img col s4" src="/webimages/CSEA.png" style="padding:1px;"></a>
-        <a href="http://minerva.nitc.ac.in/cse/"></a><img class="responsive-img col s4" src="/webimages/CSED.png" style="padding:1px;"></a>
-      </div>
-      <div class="col l4 m6 s12 center" >
-        <span style="font-family:'webfontregular'; font-weight:350; color: #FFFFFF; font-size:40px;vertical-align:center;">threads.</span>
-      </div>
-        <div class="col l4 m6 s12 center">
-        <div class="">
-      <div class="col s12">
-        <img class="responsive-img" src="/webimages/logo.png">
-      </div>
-    
-            </div>
- 
- 
- 
- 
- 
- 
- 
- 
-        </div>
- 
- 
- 
- 
-    </div>
-      <div class="col s12">
-        <br>
-        <hr>
-      </div>
-      <div class="col l12 m12 s12 center">
-                <img class="responsive-img" src="/webimages/copyright.png">
-        </div>
- 
-  </div>
-  </div>
-    
-
-		<script>
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    options={};
-    var instances = M.Dropdown.init(elems, options);
-  });
-			/* 
-     var dropdown = document.getElementsByClassName("dropdown-btn");
-			var i;
-			
-			for (i = 0; i < dropdown.length; i++) {
-			  dropdown[i].addEventListener("click", function() {
-			    this.classList.toggle("active");
-			    var dropdownContent = this.nextElementSibling;
-			    var current = document.getElementsByClassName("selected");
-			    if(current.length>0){
-			    current[0].className = current[0].className.replace(" sidenav-selected", "");}
-			    this.className += " sidenav-selected"; //<!--Dude, I copied and refined the code from www.w3schools.com/howto/tryit.asp?filename=tryhow_js_active_element  but it is not working, can you check?-->
-			    if (dropdownContent.style.display === "block") {
-			      dropdownContent.style.display = "none";
-			   
-			    } else {
-			      dropdownContent.style.display = "block";
-			    
-			    }
-			  });
-			}*/
-			
-			function loaddummy(data)
-			{
-				
-				//{}
-						 
-					var article = data;
-					var i;
-					$('#title-box').empty();
-					
-					//$('#content-box').empty();
-					
-					$('#title-box').html(article.title);
-					$('#viewcount').html(article.viewcount+" Views");
-					$('#content-box').html(article.content);
-					$('#authorbio').html(article.authorshortbio);
-			
-				$('#authorname').html(article.authorname);
-					if(article.authorname!="nil"){
-					jQuery("#authorphoto").attr("src", article.authorphoto) 
-					} 
-			}
-
-			function loadarticle(id) {
-				return;
-        $('.navbaritem-selected').removeClass().addClass("navbaritem");
-        $('[eid='+id+']').removeClass();
-        $('[eid='+id+']').addClass("navbaritem-selected")
-				/*
-  
-				<div class="article" id="article">
-				  
-				  <!--- to render article -->
-				  
-				  <div id="articletitle">
-				    
-				  </div>
-				  
-				  <div id="content">
-				    
-				  </div>
-				   
 				</div>
-  
-  
-				*/
- 
-				$.get("/getarticle.php?id=" + id, function(data) {
-					var article = JSON.parse(data);
-					var i;
-					$('#title-box').empty();
-					
-					$('#content-box').empty();
-					
-					$('#title-box').html(article.title);
-					$('#viewcount').html(article.viewcount+" Views");
-					$('#content-box').html(article.content);
-					$('#authorbio').html(article.authorshortbio);
-				$('#authorname').html(article.authorname);
-					if(article.authorname!="nil"){
-					jQuery("#authorphoto").attr("src", article.authorphoto) 
-					}
-				//	$('#authorbio').append(div);
-				});
-				$('#article-box').show();
-				$('#articles').hide();
-				
-			
-			}
-			function loadarticlelist(id,loadfirstarticle) {
-			 
-        
-      
-        
-        $('#navbarentry-wrap').html('');
-				$.get("../getarticlelist.php?id=" + id, function(data) {
 
-					var articles = JSON.parse(data);
-					
-					var i;
-					 var ind;
-					for (i = 0; i < articles.length; i++) { 
-						var title = $(document.createElement('div')).attr({eid:articles[i].id,onclick:"loadarticle("+articles[i].id+");"});
-						if(loadfirstarticle&&i==0){
-            title.addClass("navbaritem-selected");
-						}
-              else{title.addClass("navbaritem");
-            }
-            
-						title.html(articles[i].title);
-						$('#navbarentry-wrap').append(title);
-		
-					}
-							if(loadfirstarticle){				
-						loadarticle(articles[0].id);
-							}
-				});
-				
-			}
-      
-		 function selectarticle(idd)
-			{
-				 loadarticle(idd)
-				
-			}
-		
-			
-			function initpage(editionid,articleid,loadfirstarticle,edname,edyear)
-			{
-      
-        $('#top-edition-text').html(edname);
-        $('#top-edition-year').html(edyear);
-				
-        loadarticlelist(editionid,loadfirstarticle);
-        if(loadfirstarticle==false){
-				          selectarticle(articleid);
-				}
-			}
-			$( document ).ready(function() {
-     
-        //load firstg current edition
-       // initpage(10,11,false,"<?php echo $curname; ?>","<?php echo $curyear; ?>");
-					
-					/*$('#title-box').html(article.title);
-					$('#viewcount').html(article.viewcount+" Views");
-					$('#content-box').html(article.content);
-					$('#authorbio').html(article.authorshortbio);
-				$('#authorname').html(article.authorname);*/
-				/*	if(article.authorname!="nil"){
-					jQuery("#authorphoto").attr("src", article.authorphoto)
-					}*/
-					<?php	$va=$_POST; ?>
-						
-						
-						
-				data={title:"<?php echo $va["title"]; ?>",
-							viewcount:0,content:"",authorshortbio:"<?php echo $va["authorbio"]; ?>",
-							authorname:"<?php echo $va["authorname"]; ?>",authorphoto:"<?php echo $va["authorphoto"]; ?>"};
-						loaddummy(data);
-					
-					});
-      
-		</script>
-	</body>
 
-	</html>
-<?php
-}
-	
-	?>
+				<div class="col s8">
+
+					<div class="row">
+						<div class="col s12">
+							<div class="article-box" id="article-box">
+								<div class="title-box" id="title-box">
+								</div>
+								<div class="authordetails" id="authordetails">
+
+									<div class="authornameandphoto">
+										<img height="70" width="70" class="circle authorphoto responsive-img" id="authorphoto" src="<?php echo $va["authorphoto"]; ?>" />
+										<div class="authortext">
+
+											<span class="authorname" id="authorname"><?php echo $va["authorname"]; ?>
+                                                    </span>
+
+
+
+
+											<div class="authorbio" id="authorbio"> <?php echo $va["authorbio"]; ?></div>
+										</div>
+										<div class="dateread">
+
+											<span class="artdate" id="artdate">
+												dummy
+											</span>
+											<span class="readmin" id="readmin">
+										<?php echo $va['minutes']; ?> min read	</span>
+
+																										 </div>
+
+										</div>
+									</div>
+
+									<div class="content-box" id="content-box"><?php echo $va["content"]; ?>
+									</div>
+									<div class="row" style="margin-top:39px;">
+
+										<div class="dot">
+
+										</div>
+										<div class="dot">
+
+										</div>
+										<div class="dot ">
+
+										</div>
+									</div>
+
+
+
+								</div>
+							</div>
+
+
+						</div>
+
+					</div>
+				</div>
+
+
+
+
+
+
+
+
+
+			</div>
+
+
+			<div class="row" style="	background: url('/webimages/BG.png');	height: 266px;	margin-bottom: 0px;	padding-top: 29px;	background-size: cover;	position: relative;">
+				<div class="row" style="padding-top:1%;">
+
+					<div class="col l3 offset-l1 m6 s12 center" style="margin-top:7px;" >
+						<a href="/"><img class="responsive-img col s4" src="/webimages/About.png" style="padding-top:7.1px;padding-right:23px;" ></a>
+						<a href="http://assoc.cse.nitc.ac.in/">		<img class="responsive-img col s4" src="/webimages/CSEA.png" style="padding:8px;padding-right:22px"></a>
+						<a href="http://minerva.nitc.ac.in/cse/"></a><img class="responsive-img col s4" src="/webimages/CSED.png" style="padding:8px;padding-right:22px"></a>
+					</div>
+					<div class="col l2 offset-l1 m6 s12 center">
+					<!--	<span style="font-family:'webfontregular'; font-weight:350; font-size:58pt;color: #FFFFFF;vertical-align:center;">threads.</span>-->
+						<img class="responsive-img" src="/webimages/threads.png" />
+					</div>
+					<div class="col l2 offset-l2 m6 s12 center">
+						<div class="">
+							<div class="col s12">
+								<img class="responsive-img"  src="/webimages/logo.png">
+							</div>
+
+						</div>
+
+
+
+
+
+
+
+
+					</div>
+
+
+
+
+				</div>
+				<div class="row s10 offset-s1 col copyrightgroup">
+					<div class="col s12" style="	margin-bottom: 30px;	height: 1px;	background-color: white;	opacity: 0.6;">
+						<hr style="margin-bottom:22px;	height: 1px;	background-color: white;	opacity: 0.6;">
+					</div>
+					<div class="col l12 m12 s12 center" style="margin-bottom:2px;">
+						<img class="responsive-img" src="/webimages/copyright.png">
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+</body>
+</html>
